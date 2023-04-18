bcb079a204f837e23b048f97db83b345b7af5e47
astricus <astricus@hotmail.com>
2023-04-18 09:25:10 +0200

    feat: libraries update, tailwind, flowbite

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-import ReactDOM from 'react-dom';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import Sandbox from './Graph';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <Sandbox />,
-  document.getElementById('app')
-);
+import './index.css';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<Sandbox />);
